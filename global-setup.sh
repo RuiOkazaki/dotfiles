602c6cab21a0a3c3c527de04f185fbe603672f65
RuiOkazaki <r_okazaki@yumemi.co.jp>
2023-10-14 17:33:03 +0900

    refactor(global-setup.sh): remove unnecessary sudo password prompt and update comments fix(global-setup.sh): fix typo in XCode command line tools installation message

--- global-setup.sh
+++ global-setup.sh
@@ -7,16 +7,6 @@ cd "$DIR"
 # scripts/functions.shをソースとして読み込みます。
 . scripts/functions.sh
 
-# sudo パスワードのプロンプトを表示します。
-info "sudo パスワードを求めています..."
-if sudo -v; then
-  # Keep-alive: `setup.sh` が終了するまで既存の `sudo` タイムスタンプを更新します。
-  while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
-  success "sudo 資格情報を更新しました。"
-else
-  error "sudo 資格情報の取得に失敗しました。"
-fi
-
 # XCodeコマンドラインツールをインストールします。
 info "XCode コマンドラインツールをインストールしています..."
 if xcode-select --print-path &>/dev/null; then
